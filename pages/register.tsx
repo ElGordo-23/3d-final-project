cba2d005425c3cbaa04787f9cc3c2ae162278288
Felix Lang <felix.lang@mailbox.org>
2021-11-21 11:55:01 +0100

    Add Login/Logout and register pages.

---
+++ pages/register.tsx
@@ -0,0 +1,122 @@
+import { GetServerSidePropsContext } from 'next';
+import { useRouter } from 'next/dist/client/router';
+import { useState } from 'react';
+import { Errors } from '../util/types';
+import { RegisterResponse } from './api/register';
+
+type Props = { csrfToken: string };
+
+export default function RegisterPage(props: Props) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState<Errors>([]);
+  const router = useRouter();
+
+  return (
+    <>
+      <h1>Register</h1>
+
+      <form
+        onSubmit={async (event) => {
+          event.preventDefault();
+
+          const registerResponse = await fetch('/api/register', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              username: username,
+              password: password,
+              csrfToken: props.csrfToken,
+            }),
+          });
+
+          const registerJson =
+            (await registerResponse.json()) as RegisterResponse;
+
+          if ('errors' in registerJson) {
+            setErrors(registerJson.errors);
+            return;
+          }
+
+          const destination =
+            typeof router.query.returnTo === 'string' && router.query.returnTo
+              ? router.query.returnTo
+              : `/`;
+
+          // props.refreshUsername();
+
+          router.push(destination);
+        }}
+      >
+        <label>
+          Username
+          <input
+            value={username}
+            onChange={(event) => setUsername(event.currentTarget.value)}
+          />
+        </label>
+        <label>
+          Password
+          <input
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.currentTarget.value)}
+          />
+        </label>
+
+        <button>Register</button>
+      </form>
+
+      <div>
+        {errors.map((error) => (
+          <div key={`error-${error.message}`}>{error.message}</div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const { getValidSessionByToken } = await import('../util/database');
+  const { createToken } = await import('../util/csrf');
+
+  // Redirect from HTTP to HTTPS on Heroku
+  if (
+    context.req.headers.host &&
+    context.req.headers['x-forwarded-proto'] &&
+    context.req.headers['x-forwarded-proto'] !== 'https'
+  ) {
+    return {
+      redirect: {
+        destination: `https://${context.req.headers.host}/register`,
+        permanent: true,
+      },
+    };
+  }
+
+  const sessionToken = context.req.cookies.sessionToken;
+
+  const session = await getValidSessionByToken(sessionToken);
+
+  console.log(session);
+
+  if (session) {
+    // Redirect the user when they have a session
+    // token by returning an object with the `redirect` prop
+    // https://nextjs.org/docs/basic-features/data-fetching#getserversideprops-server-side-rendering
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      csrfToken: createToken(),
+    },
+  };
+}
