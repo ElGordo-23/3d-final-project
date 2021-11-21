cae22e91e70ed89e91305d19df70b5aeae6a374b
Felix Lang <felix.lang@mailbox.org>
2021-11-21 11:52:49 +0100

    Add global styles.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import { css, Global } from '@emotion/react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Global
+        styles={css`
+          @import url('https://fonts.googleapis.com/css?family=Oswald:500');
+          *,
+          *::before,
+          *::after {
+            box-sizing: border-box;
+          }
+          body {
+            margin: 0;
+          }
+        `}
+      />
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
